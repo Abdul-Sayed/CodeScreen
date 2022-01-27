7f47a01ba6645fb3d17239c93beeb0ce2931bbd0
Abdul Sayed <awsayed@alaska.edu>
2022-01-26 20:25:44 -0800

    adding solution for first class

--- tweets-api-service.js
+++ tweets-api-service.js
@@ -1,17 +1,35 @@
+import fetch from "node-fetch";
 export class TweetsAPIService {
+  static tweetsEndpointUrl = "https://app.codescreen.com/api/assessments/tweets";
 
-    static tweetsEndpointUrl = "https://app.codescreen.com/api/assessments/tweets";
+  // Your API token. Needed to successfully authenticate when calling the tweets endpoint.
+  // This needs to be included in the Authorization header (using the Bearer authentication scheme) in the request you send to the tweets endpoint.
+  static apiToken = "8c5996d5-fb89-46c9-8821-7063cfbc18b1";
 
-    // Your API token. Needed to successfully authenticate when calling the tweets endpoint. 
-    // This needs to be included in the Authorization header (using the Bearer authentication scheme) in the request you send to the tweets endpoint.
-    static apiToken = "8c5996d5-fb89-46c9-8821-7063cfbc18b1";
+  /**
+   * Retrieves all tweets for the given user by calling the https://app.codescreen.com/api/assessments/tweets endpoint.
+   * The username should be passed in the request as a query parameter called userName.
+   */
+  async getTweets(userName) {
+    //TODO Implement...
 
-    /**
-     * Retrieves all tweets for the given user by calling the https://app.codescreen.com/api/assessments/tweets endpoint.
-     * The username should be passed in the request as a query parameter called userName.
-    */
-    async getTweets(userName) {
-        //TODO Implement...
-    }
-
+    return fetch("https://app.codescreen.com/api/assessments/tweets" + "?userName=" + userName, {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer 8c5996d5-fb89-46c9-8821-7063cfbc18b1",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
 }
--- tweets-data-stats-generator.js
+++ tweets-data-stats-generator.js
@@ -1,45 +1,63 @@
+import { TweetsAPIService } from "./tweets-api-service.js";
+import moment from "moment";
+
+const tweetsApi = new TweetsAPIService();
 export class TweetsDataStatsGenerator {
+  constructor(tweetsApiService) {
+    this.tweetsApiService = tweetsApiService;
+  }
 
-    constructor(tweetsApiService) {
-        this.tweetsApiService = tweetsApiService;
-    }
+  /**
+   * Retrieves the highest number of tweets that were created on any given day by the given user.
+   * A day's time period here is defined from 00:00:00 to 23:59:59
+   * If there are no tweets for the given user, this method should return 0.
+   */
+  async getMostTweetsForAnyDay(userName) {
+    //TODO Implement...
+    const tweetsArr = await tweetsApi.getTweets(userName);
+    // console.log(JSON.stringify(tweetsArr, null, "\t"));
 
-    /**
-     * Retrieves the highest number of tweets that were created on any given day by the given user.
-     * A day's time period here is defined from 00:00:00 to 23:59:59
-     * If there are no tweets for the given user, this method should return 0.
-    */
-   async getMostTweetsForAnyDay(userName) {
-        //TODO Implement...
-    }
+    let countArr = [];
+    tweetsArr.forEach((tweet) => {
+      let tweetDay = tweet.createdAt.substr(0, 13);
+      countArr.push(
+        tweetsArr.filter((tweetObj) => {
+          return tweetObj.createdAt.substr(0, 13) === tweetDay;
+        }).length
+      );
+    });
+    return Math.max(...countArr);
+  }
 
-    /**
-     * Finds the ID of longest tweet for the given user.
-     * You can assume there will only be one tweet that is the longest.
-     * If there are no tweets for the given user, this method should return null.
-    */
-   async getLongestTweet(userName) {
-        //TODO Implement...
-    }
+  /**
+   * Finds the ID of longest tweet for the given user.
+   * You can assume there will only be one tweet that is the longest.
+   * If there are no tweets for the given user, this method should return null.
+   */
+  async getLongestTweet(userName) {
+    //TODO Implement...
+  }
 
-    /**
-     * Retrieves the most number of days between tweets by the given user.
-     * This should always be rounded down to the complete number of days, i.e. if the time is 12 days & 3 hours, this
-     * method should return 12.
-     * If there are no tweets for the given user, this method should return null.
-    */
-   async findMostDaysBetweenTweets(userName) {
-        //TODO Implement...
-    }
+  /**
+   * Retrieves the most number of days between tweets by the given user.
+   * This should always be rounded down to the complete number of days, i.e. if the time is 12 days & 3 hours, this
+   * method should return 12.
+   * If there are no tweets for the given user, this method should return null.
+   */
+  async findMostDaysBetweenTweets(userName) {
+    //TODO Implement...
+  }
 
-    /**
-     * Retrieves the most popular hash tag tweeted by the given user.
-     * Note that the string returned by this method should include the hashtag itself.
-     * For example, if the most popular hash tag is "#JavaScript", this method should return "#JavaScript".
-     * If there are no tweets for the given user, this method should return null.
-    */
-   async getMostPopularHashTag(userName) {
-        //TODO Implement...
-    }   
-    
+  /**
+   * Retrieves the most popular hash tag tweeted by the given user.
+   * Note that the string returned by this method should include the hashtag itself.
+   * For example, if the most popular hash tag is "#JavaScript", this method should return "#JavaScript".
+   * If there are no tweets for the given user, this method should return null.
+   */
+  async getMostPopularHashTag(userName) {
+    //TODO Implement...
+  }
 }
+
+let tweetsApiGen = new TweetsDataStatsGenerator();
+tweetsApiGen.getMostTweetsForAnyDay("joe_smith");
